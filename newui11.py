bde47556998ae5d6659cc2d8c574f620aff12b1f
scooter7 <scooter.vineburgh@gmail.com>
2023-04-24 23:11:02 -0500

    Update newui11.py

--- newui11.py
+++ newui11.py
@@ -7,12 +7,11 @@ from datetime import datetime
 
 # Everything is accessible via the st.secrets dict:
 st.write("DB username:", st.secrets["db_username"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 # And the root-level secrets are also accessible as environment variables:
 import os
 st.write(
 	"Has environment variables been set:",
-	os.environ["db_username"] == st.secrets["db_username"])
+	os.environ["db_username"] == st.secrets["value"])
 	
 
 
