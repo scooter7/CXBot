6f98af6c1f018e28787e1c8d63c06163bc7ab9f5
scooter7 <scooter.vineburgh@gmail.com>
2023-08-19 21:03:30 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -3,13 +3,7 @@ import streamlit as st
 import sys
 import requests
 
-if "OPENAI_API_KEY" not in st.secrets:
-    st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
-    sys.exit(1)
-
 openai_api_key = st.secrets["OPENAI_API_KEY"]
-
-# Set up the GitHub API
 g = Github(st.secrets["GITHUB_TOKEN"])
 repo = g.get_repo("scooter7/CXBot")
 
@@ -33,23 +27,9 @@ with st.container():
         st.write("You:", st.session_state.responses[i])
 
 def get_followup_question(response, question):
-    headers = {
-        'Authorization': f'Bearer {openai_api_key}',
-        'Content-Type': 'application/json',
-    }
-    
+    headers = {'Authorization': f'Bearer {openai_api_key}', 'Content-Type': 'application/json'}
     framed_prompt = f"The user was asked: '{question}'. They replied: '{response}'. What would be a good follow-up question?"
-    
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": framed_prompt},
-            {"role": "assistant", "content": ""}
-        ],
-        "temperature": 0.7
-    }
-    
+    data = {"model": "gpt-3.5-turbo", "messages": [{"role": "system", "content": "You are a helpful assistant."},{"role": "user", "content": framed_prompt},{"role": "assistant", "content": ""}],"temperature": 0.7}
     response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=data)
     follow_up = response.json()['choices'][0]['message']['content'].strip()
     return follow_up.replace("A good follow-up question could be:", "").strip()
@@ -68,6 +48,10 @@ def save_chat_history():
     chat_history = "\n".join([f"Bot: {questions[i // 2] if i % 2 == 0 else st.session_state.follow_ups[i // 2]}\nYou: {resp}" for i, resp in enumerate(st.session_state.responses)])
     demographics_data = "\n".join([f"{key}: {value}" for key, value in st.session_state.demographics.items()])
     complete_history = f"{chat_history}\n\n--- Demographics ---\n{demographics_data}"
+    try:
+        repo.get_dir_contents("content")
+    except:
+        repo.create_git_ref(ref=f"refs/heads/content", sha=repo.get_git_ref("heads/master").object.sha)
     repo.create_file(f"content/chat_history_{len(st.session_state.responses)}.txt", "Add chat history", complete_history)
 
 if st.session_state.current_question_index < len(questions):
@@ -78,21 +62,10 @@ else:
     st.subheader("We just need a bit more information, especially if you are eligible for an incentive.")
     st.session_state.demographics['Full Name'] = st.text_input("Full Name:")
     st.session_state.demographics['Email Address'] = st.text_input("Email Address:")
-    st.session_state.demographics['Gender Identity'] = st.selectbox("Which of these best describes your current gender identity?", [
-        "Cisgender female/woman", "Cisgender male/man", "Non-binary", 
-        "Transgender female/woman", "Transgender male/man", "A gender not listed here", "Prefer to not say"
-    ])
-    st.session_state.demographics['Age'] = st.selectbox("Age", [
-        "17 or under", "18-24 years old", "25-34 years old", "35-44 years old", 
-        "45-54 years old", "55-64 years old", "65-74 years old", "75 years or older"
-    ])
-    st.session_state.demographics['Ethnicity'] = st.selectbox("Which best describes you?", [
-        "Asian or Asian American", "Black or African American", "Hispanic, Latino, or Spanish", 
-        "Middle Eastern", "White or Caucasian", "North American Indigenous", 
-        "Hawaiian native or Pacific Islander", "Other", "Prefer to not say"
-    ])
+    st.session_state.demographics['Gender Identity'] = st.selectbox("Which of these best describes your current gender identity?", ["Cisgender female/woman", "Cisgender male/man", "Non-binary", "Transgender female/woman", "Transgender male/man", "A gender not listed here", "Prefer to not say"])
+    st.session_state.demographics['Age'] = st.selectbox("Age", ["17 or under", "18-24 years old", "25-34 years old", "35-44 years old", "45-54 years old", "55-64 years old", "65-74 years old", "75 years or older"])
+    st.session_state.demographics['Ethnicity'] = st.selectbox("Which best describes you?", ["Asian or Asian American", "Black or African American", "Hispanic, Latino, or Spanish", "Middle Eastern", "White or Caucasian", "North American Indigenous", "Hawaiian native or Pacific Islander", "Other", "Prefer to not say"])
     st.session_state.demographics['Zip Code'] = st.text_input("What is your 5-digit zip code (if you live in the United States)?")
-
     if st.button("Finish"):
         save_chat_history()
         st.write("Thank You!")
