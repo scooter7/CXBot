3c2bb2007f73e066de57c9c6fcaa3297261a930a
scooter7 <scooter.vineburgh@gmail.com>
2023-08-19 20:52:51 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -1,3 +1,4 @@
+from github import Github
 import streamlit as st
 import sys
 import requests
@@ -8,6 +9,10 @@ if "OPENAI_API_KEY" not in st.secrets:
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
+# Set up the GitHub API
+g = Github(st.secrets["GITHUB_TOKEN"])
+repo = g.get_repo("scooter7/CXBot")
+
 questions = [
     'Q1: Why did you visit our website today?',
     'Q2: Where are you in your college decision process?',
@@ -17,6 +22,7 @@ questions = [
 st.session_state.setdefault('current_question_index', 0)
 st.session_state.setdefault('responses', [])
 st.session_state.setdefault('follow_ups', [])
+st.session_state.setdefault('demographics', {})
 
 st.title("Survey QA Bot")
 
@@ -58,25 +64,33 @@ def handle_input():
         st.session_state.current_question_index += 1
     st.session_state.user_input = ""
 
+def save_chat_history():
+    chat_history = "\n".join([f"Bot: {questions[i // 2] if i % 2 == 0 else st.session_state.follow_ups[i // 2]}\nYou: {resp}" for i, resp in enumerate(st.session_state.responses)])
+    repo.create_file(f"content/chat_history_{len(st.session_state.responses)}.txt", "Add chat history", chat_history)
+
 if st.session_state.current_question_index < len(questions):
     next_question = questions[st.session_state.current_question_index] if len(st.session_state.responses) % 2 == 0 else st.session_state.follow_ups[-1]
     st.write("Bot:", next_question)
     st.text_input("Your Response:", value=st.session_state.get('user_input', ''), on_change=handle_input, key="user_input")
 else:
     st.subheader("We just need a bit more information, especially if you are eligible for an incentive.")
-    st.text_input("Full Name:")
-    st.text_input("Email Address:")
-    st.selectbox("Which of these best describes your current gender identity?", [
+    st.session_state.demographics['Full Name'] = st.text_input("Full Name:")
+    st.session_state.demographics['Email Address'] = st.text_input("Email Address:")
+    st.session_state.demographics['Gender Identity'] = st.selectbox("Which of these best describes your current gender identity?", [
         "Cisgender female/woman", "Cisgender male/man", "Non-binary", 
         "Transgender female/woman", "Transgender male/man", "A gender not listed here", "Prefer to not say"
     ])
-    st.selectbox("Age", [
+    st.session_state.demographics['Age'] = st.selectbox("Age", [
         "17 or under", "18-24 years old", "25-34 years old", "35-44 years old", 
         "45-54 years old", "55-64 years old", "65-74 years old", "75 years or older"
     ])
-    st.selectbox("Which best describes you?", [
+    st.session_state.demographics['Ethnicity'] = st.selectbox("Which best describes you?", [
         "Asian or Asian American", "Black or African American", "Hispanic, Latino, or Spanish", 
         "Middle Eastern", "White or Caucasian", "North American Indigenous", 
         "Hawaiian native or Pacific Islander", "Other", "Prefer to not say"
     ])
-    st.text_input("What is your 5-digit zip code (if you live in the United States)?")
+    st.session_state.demographics['Zip Code'] = st.text_input("What is your 5-digit zip code (if you live in the United States)?")
+
+    if st.button("Finish"):
+        save_chat_history()
+        st.write("Thank You!")
