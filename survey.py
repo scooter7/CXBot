8d206396c1263dc91d8083f5b154b852dbf91002
scooter7 <scooter.vineburgh@gmail.com>
2023-08-19 19:14:27 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -1,19 +1,12 @@
 import streamlit as st
 import sys
-from github import Github
 import requests
 
 if "OPENAI_API_KEY" not in st.secrets:
     st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
     sys.exit(1)
 
-if "GITHUB_TOKEN" not in st.secrets:
-    st.error("Please set the GITHUB_TOKEN secret on the Streamlit dashboard.")
-    sys.exit(1)
-
 openai_api_key = st.secrets["OPENAI_API_KEY"]
-g = Github(st.secrets["GITHUB_TOKEN"])
-repo = g.get_repo("scooter7/CXBot")
 
 questions = [
     'Q1: Why did you visit our website today?',
@@ -38,15 +31,6 @@ with st.container():
         st.write("Bot:", question)
     for follow_up in st.session_state.follow_ups:
         st.write("Bot:", follow_up)
-
-def save_chat_history_to_github():
-    chat_history = ""
-    for response, question in zip(st.session_state.responses, st.session_state.questions[1:]):
-        chat_history += f"You: {response}\n"
-        chat_history += f"Bot: {question}\n"
-    for follow_up in st.session_state.follow_ups:
-        chat_history += f"Bot: {follow_up}\n"
-    repo.create_file(f"content/chat_history_{len(st.session_state.responses)}.txt", "Add chat history", chat_history)
 
 def get_followup_question(response, question):
     headers = {
@@ -74,7 +58,6 @@ def handle_input():
     st.session_state.responses.append(user_input)
     follow_up = get_followup_question(user_input, st.session_state.questions[len(st.session_state.responses) - 1])
     st.session_state.follow_ups.append(follow_up)
-    save_chat_history_to_github()
 
 if len(st.session_state.questions) > len(st.session_state.responses):
     next_question = st.session_state.questions[len(st.session_state.responses)]
