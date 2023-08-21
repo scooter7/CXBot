851988c3eeee3722fc2bd976379745419fdbc0b7
scooter7 <scooter.vineburgh@gmail.com>
2023-08-20 19:36:10 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -31,15 +31,13 @@ def get_followup_question(response, question):
     follow_up = response.json()['choices'][0]['message']['content'].strip()
     return follow_up.replace("A good follow-up question could be:", "").strip()
 
-def handle_input():
-    user_input = st.session_state.user_input
+def handle_input(user_input):
     st.session_state.responses.append(user_input)
     if len(st.session_state.responses) % 2 == 1:
         follow_up = get_followup_question(user_input, questions[st.session_state.current_question_index])
         st.session_state.follow_ups.append(follow_up)
     else:
         st.session_state.current_question_index += 1
-    st.session_state.user_input = ""
 
 def save_chat_history():
     chat_history = "\n".join([f"Bot: {questions[i // 2] if i % 2 == 0 else st.session_state.follow_ups[i // 2]}\nYou: {resp}" for i, resp in enumerate(st.session_state.responses)])
@@ -54,7 +52,7 @@ if st.session_state.current_question_index < len(questions):
     st.write("Bot:", next_question)
     user_input = st.text_input("Your Response:", value=st.session_state.get('user_input', ''), key="user_input")
     if st.button("Submit"):
-        handle_input()  # Call handle_input without arguments
+        handle_input(user_input)
 else:
     st.subheader("We just need a bit more information, especially if you are eligible for an incentive.")
     st.session_state.demographics['Full Name'] = st.text_input("Full Name:")
